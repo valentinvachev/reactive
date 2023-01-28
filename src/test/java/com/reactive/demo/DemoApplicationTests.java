c7041150842e774b4e5c72664c8ab625be36a86e
vachev <v.vachevstz@gmail.com>
2023-01-28 12:29:41 -0800

    Test

--- src/test/java/com/reactive/demo/DemoApplicationTests.java
+++ src/test/java/com/reactive/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.reactive.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,6 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
-
 }
